3813652f624d1898846ae68aad7600e2d28f13d8
Kryptyk Physh <kryptykphysh@kryptykphysh.xyz>
2018-01-24 23:56:13 +0000

    Rename app after copy.

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ require 'sprockets/railtie'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module BaseRailsApp
+module DemoApp
   # Top level application class
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
